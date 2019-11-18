d945bf927ff53f1f3f2bc6adf689ef6cdeee7919
Gustavo de Leon <gdeleontobias@gmail.com>
2019-11-17 19:33:22 -0600

    realiza pasos automaticos solo a la derecha

--- maquina/maquina.py
+++ maquina/maquina.py
@@ -1,7 +1,6 @@
 f = open("MT1.txt")
 
 cinta_inicial = ""
-
 cinta1 = ""
 cinta2 = ""
 cinta3 = ""
@@ -11,43 +10,105 @@ cintaz = ""
 for line in f:
     cinta_inicial = cinta_inicial + line
 
-def leer_cinta_inicial(cinta):
-
+def paso_inicial(cinta):
     cont = cinta.find("111")
     cinta1 = "q"+cinta[:cont+3]
     cinta2 = "q01"+cinta[cont+3:]
     cinta3 = "q"
     cintaz = cinta[4:cont+3]
+    print ("PASO 1 Y 2")
+    print("-------------------------------------------------------------------")
+    print ("Cinta 1")
+    print(cinta1)
+    print ("Cinta 2")
+    print(cinta2)
+    print ("Cinta 3")
+    print(cinta3)
 
     return cinta1, cinta2, cinta3, cintaz
 
-def quitar(cinta1, cinta2, cinta3):
+def paso3(cinta1, cinta2, cinta3):
     cinta3 = cinta3 + cinta1[cinta1.find("q")+1:cinta1.find("1")]
     cinta1 = cinta1[cinta1.find("q")]+cinta1[cinta1.find("1")+1:]
+    print("-------------------------------------------------------------------")
+    print ("PASO 3")
+    print("-------------------------------------------------------------------")
+    print ("Cinta 1")
+    print(cinta1)
+    print ("Cinta 2")
+    print(cinta2)
+    print ("Cinta 3")
+    print(cinta3)
+    
     return cinta1, cinta2, cinta3
 
 
-def alo(cinta1,zonas_establecidas):
-    ubc = str(cinta1.find("q"))
-    #print("Q se encuentra en la seccion " + ubc)
-    #print("Se buscara en la zona " + ubc)
-    est = zonas_establecidas[int(ubc)].estado1
-    let = zonas_establecidas[int(ubc)].letra1
-    #print("el estado es: "+est)
-    #print("la letra es: "+let)
-    print("Busca la transicion con estado "+est+ " y letra "+let+ " en la cinta 1")
-    cinta1 = cinta1.replace("q","")
-    ss = len(est+"1"+let+"1")
-    #print("**************************************")
-    #print(ss)
-    #print(cinta1)
-    #print("**************************************")
-    var = cinta1.find(est+"1"+let)+ss 
-    cinta1 = cinta1[:var]+"q"+cinta1[var:]
+def maquina(cinta1,cinta2,cinta3,zonas_establecidas):
+    cont = 0
+    #CICLO MOMENTANEO MIENTRAS SE CREA LA CONDICION PARA SALIR DEL LOOP
+    while(cont < 3):
+        #INICIO DE PASOS REPETITIVOS
 
-    return (cinta1)
+        #PRIMERA PARTE DE LOS PASOS REPETITIVOS
+        print("-------------------------------------------------------------------")
+        print ("PASO "+str(cont+4))
+        print("-------------------------------------------------------------------")
+        #OBTIENE EL ESTADO CORRESPONDIENTE
+        estadoC = cinta3[1:]
+        #OBTIENE LA LETRA CORRESPONDIENTE
+        q1 = cinta2.find("q")+1
+        enc = cinta2[q1:].find("1")
+        letraC = cinta2[q1:][:enc]
+        print("Busca la transicion con estado "+estadoC+ " y letra "+letraC+ " en la cinta 1")
+        #MUEVE LA LETRA Q A SU POSICION NUEVA
+        cinta1 = cinta1.replace("q","")
+        ss = len(estadoC+"1"+letraC+"1")
+        var = cinta1.find(estadoC+"1"+letraC)+ss
+        #IMPRIME NUEVOS VALORES DE LAS CINTAS
+        cinta1 = cinta1[:var]+"q"+cinta1[var:]
+        print ("Cinta 1")
+        print(cinta1)
+        print ("Cinta 2")
+        print(cinta2)
+        print ("Cinta 3")
+        print(cinta3)
+        print("")
+        #SEGUNDA PARTE DE LOS PASOS REPETITIVOS
+        print("Despues de la busqueda de la transicion con estado "+estadoC+ " y letra "+letraC+ " en la cinta 1")
 
+        #NUEVO ESTADO DE CINTA 3
+        encq = cinta1.find("q")
+        cinta1N = cinta1[encq:]
+        encq1 =cinta1N.find("1")
+        estadoN = cinta1N[:encq1]
+        cinta3 = estadoN
 
+        #REINICIO DE CINTA 1 
+        cinta1 = cinta1.replace("q","")
+        cinta1 = "q"+cinta1
+        
+        #MOVIMIENTO DE Q EN CINTA 2 (POR EL MOMENTO SOLO A LA DERECHA)
+        encq2 = cinta2.find("q")
+        encq3 = cinta2.find("1")
+        aaaa = cinta2[encq2:].find("1")
+        bbb = aaaa+encq2
+        cinta2 = cinta2.replace("q","")
+        cinta2 = cinta2[:bbb]+"q"+cinta2[bbb:]
+
+        
+        #IMPRIME NUEVOS VALORES DE LAS CINTAS
+        print ("Cinta 1")
+        print(cinta1)
+        print ("Cinta 2")
+        print(cinta2)
+        print ("Cinta 3")
+        print(cinta3)
+        print("")
+        #CONTADOR MOMENTANEO MIENTRAS SE CREA CONDICION DE FINALIZACION
+        cont +=1
+    
+    return cinta1,cinta2,cinta3
+    
 def separar_zonas(cinta):
 
     global contador
@@ -93,10 +154,7 @@ def crear_objetos(lista_de_zonas):
 
     for i in range(0, len(lista_de_zonas)):
         separaciones = lista_de_zonas[i].split('1')
-        #print(separaciones)
-
         objeto = Zona(separaciones[0], separaciones[1], separaciones[2], separaciones[3], separaciones[4])
-
         lista_objetos.append(objeto)
 
     return lista_objetos
@@ -105,48 +163,9 @@ print("-------------------------------------------------------------------")
 print ("Cinta inicial")
 print (cinta_inicial)
 print("-------------------------------------------------------------------")
-cinta1, cinta2, cinta3, cintaz = leer_cinta_inicial(cinta_inicial)
-print ("PASO 1 Y 2")
-print("-------------------------------------------------------------------")
-print ("Cinta 1")
-print(cinta1)
-print ("Cinta 2")
-print(cinta2)
-print ("Cinta 3")
-print(cinta3)
-print("-------------------------------------------------------------------")
-print ("PASO 3")
-
-cinta1, cinta2, cinta3 = quitar(cinta1, cinta2, cinta3)
-
-print("-------------------------------------------------------------------")
-print ("Cinta 1")
-print(cinta1)
-print ("Cinta 2")
-print(cinta2)
-print ("Cinta 3")
-print(cinta3)
-#print("-------------------------------------------------------------------")
-#print('CINTA Z')
-#print(cintaz)
-#print("-------------------------------------------------------------------")
-
+cinta1, cinta2, cinta3, cintaz = paso_inicial(cinta_inicial)
+cinta1, cinta2, cinta3 = paso3(cinta1, cinta2, cinta3)
 zonas = separar_zonas(cintaz)
-
 zonas_establecidas = crear_objetos(zonas)
+inta1,cinta2,cinta3 = maquina(cinta1,cinta2,cinta3,zonas_establecidas)
 
-
-#for x in zonas_establecidas:
-#    print(x.estado1, x.letra1, x.estado2, x.letra2, x.movimiento)
-print("-------------------------------------------------------------------")
-#print(zonas_establecidas[2].movimiento)
-print ("PASO 4")
-print("-------------------------------------------------------------------")
-cinta1 = alo(cinta1,zonas_establecidas)
-print ("Cinta 1")
-print(cinta1)
-print ("Cinta 2")
-print(cinta2)
-print ("Cinta 3")
-print(cinta3)
-print("-------------------------------------------------------------------")
