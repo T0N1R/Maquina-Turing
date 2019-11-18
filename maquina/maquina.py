8a032bf33fea1d051fd260a62a39d4d3a6c29116
Gustavo de Leon <gdeleontobias@gmail.com>
2019-11-18 00:13:39 -0600

    cambios finales

--- maquina/maquina.py
+++ maquina/maquina.py
@@ -46,7 +46,7 @@ def paso3(cinta1, cinta2, cinta3):
 def maquina(cinta1,cinta2,cinta3,zonas_establecidas):
     cont = 0
     #CICLO MOMENTANEO MIENTRAS SE CREA LA CONDICION PARA SALIR DEL LOOP
-    while(cont < 4):
+    while(cont < 7):
         #INICIO DE PASOS REPETITIVOS
 
         #PRIMERA PARTE DE LOS PASOS REPETITIVOS
@@ -55,22 +55,51 @@ def maquina(cinta1,cinta2,cinta3,zonas_establecidas):
         print("-------------------------------------------------------------------")
         #OBTIENE EL ESTADO CORRESPONDIENTE
         estadoC = cinta3[1:]
+        
         #OBTIENE LA LETRA CORRESPONDIENTE
         q1 = cinta2.find("q")+1
         enc = cinta2[q1:].find("1")
         letraC1 = cinta2[q1:][:enc]+"1"
         letraC = cinta2[q1:][:enc]
         print("Busca la transicion con estado "+estadoC+ " y letra "+letraC+ " en la cinta 1")
+        
         #MUEVE LA LETRA Q A SU POSICION NUEVA
         cinta1 = cinta1.replace("q","")
         ss = len(estadoC+"1"+letraC1)
         var = cinta1.find(estadoC+"1"+letraC1)+ss
-        #print("******************************************")
-        #print(cinta1[:var])
-        #print(cinta1[var:])
-        #print("******************************************")
+        cochinada = cinta1.find(estadoC+"1"+letraC1) #da la posicion de lo que se busca
+        valor_anterior = cinta1[cochinada-2:cochinada] #Para saber si es estado1 siendo = a 11
+
+        if(cochinada != 0):
+            #print("El estado no es el primero")
+            if(valor_anterior=="01"):
+                #print("esta mal")
+                anterior = cinta1[:var]
+                correccion = cinta1[var:]
+                ss = len(estadoC+"1"+letraC1)
+                var = correccion.find(estadoC+"1"+letraC1)+ss
+                correccion = correccion[:var]+"q"+correccion[var:]
+                cinta1 = anterior + correccion
+                #print("BUENA")
+            else:
+                #print("esta bien")
+                cinta1 = cinta1[:var]+"q"+cinta1[var:]
+        else:
+            cinta1 = cinta1[:var]+"q"+cinta1[var:]
+
         #IMPRIME NUEVOS VALORES DE LAS CINTAS
-        cinta1 = cinta1[:var]+"q"+cinta1[var:]
+
+        salvacion = cinta1.find("q")
+        salva = cinta1[salvacion:]
+        num = salva.find("11")
+        vida = num+salvacion
+        busca = cinta1[salvacion+1:vida]
+        busca1 = busca.find("1")
+        busca2 = busca[busca1+1:]
+        busca3 = busca2.find("1")
+        busca4 = busca2[busca3+1:]
+        movimiento = busca4
+
         print ("Cinta 1")
         print(cinta1)
         print ("Cinta 2")
@@ -98,13 +127,44 @@ def maquina(cinta1,cinta2,cinta3,zonas_establecidas):
         cinta1 = cinta1.replace("q","")
         cinta1 = "q"+cinta1
         
-        #MOVIMIENTO DE Q EN CINTA 2 (POR EL MOMENTO SOLO A LA DERECHA)
-        encq2 = cinta2.find("q")
-        encq3 = cinta2.find("1")
-        aaaa = cinta2[encq2:].find("1")
-        bbb = aaaa+encq2
-        cinta2 = cinta2.replace("q","")
-        cinta2 = cinta2[:bbb]+"q"+cinta2[bbb:]
+        #MOVIMIENTO DE Q EN CINTA 2
+        if(movimiento == "000"):
+            encq2 = cinta2.find("q")
+            encq3 = cinta2.find("1")
+            aaaa = cinta2[encq2:].find("1")
+            bbb = aaaa+encq2
+            cinta2 = cinta2.replace("q","")
+            cinta2 = cinta2[:bbb]+"q"+cinta2[bbb:]
+            
+        elif(movimiento == "00"):
+            numq = cinta2.find("q")
+            if(numq == 2):
+                print("hay que moverse")
+                cinta2 = cinta2.replace("q","")
+                cinta2 = "q"+cinta2
+            else:
+                conter = 0
+                conti = 0
+                contai = 0
+                contaG = 0
+                for x in cinta2:
+                    if(x == "1"):
+                        conter+=1
+                        if(cinta2[conti+1] =="q"):
+                            break
+                    conti +=1
+                cinta2 = cinta2.replace("q","")
+                
+                
+                for z in cinta2:
+                    contaG+=1
+                    if(z == "1"):
+                        contai+=1
+                    if(contai == (conter-1)):
+                        break
+                cinta2 = cinta2[:contaG]+"q"+cinta2[contaG:] 
+        else:
+            print("No te moves")
 
         
         #IMPRIME NUEVOS VALORES DE LAS CINTAS
@@ -178,5 +238,4 @@ cinta1, cinta2, cinta3, cintaz = paso_inicial(cinta_inicial)
 cinta1, cinta2, cinta3 = paso3(cinta1, cinta2, cinta3)
 zonas = separar_zonas(cintaz)
 zonas_establecidas = crear_objetos(zonas)
-inta1,cinta2,cinta3 = maquina(cinta1,cinta2,cinta3,zonas_establecidas)
-
+cinta1,cinta2,cinta3 = maquina(cinta1,cinta2,cinta3,zonas_establecidas)
